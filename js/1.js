236115c47b320aa3fae970e8835bb0cc926039a9
VitalikGrischenko <110487381+VitalikGrischenko@users.noreply.github.com>
2023-04-06 19:27:34 +0300

    ркрго

---
+++ js/1.js
@@ -0,0 +1,7 @@
+const b6 = 65;  //глобальна зміна
+if(true){
+    console.log(b6);
+    const c95 = 548;   //локальна
+}
+
+console.log(c95)
---
+++ js/2.js
@@ -0,0 +1,18 @@
+let lang = 'ua'; 'en'; 'ru';'fr';
+
+switch (lang) {
+  case 'ua':
+    console.log('Січень');
+    break;
+  case 'en':
+    console.log('January');
+    break;
+  case 'ru':
+    console.log('Январь');
+    break;
+  case 'fr':
+    console.log('Janvier');
+    break;
+  default:
+    console.log('Невідома мова')
+}
